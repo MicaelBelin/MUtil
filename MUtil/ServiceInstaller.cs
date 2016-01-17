0a6913fa11687746bcd41ee64f8edf097109e4e6
Micael Belin <micael.belin@gmail.com>
2016-01-17 22:58:58 +0100

    Added support for interactive console.

--- MUtil/ServiceInstaller.cs
+++ MUtil/ServiceInstaller.cs
@@ -41,162 +41,335 @@ namespace Xintric.MUtil
             this.Installers.Add(serviceInstaller);
         }
 
+        public static void RunService(System.ServiceProcess.ServiceBase service, string[] args)
+        {
+            var method = service.GetType().GetMethod("OnStart", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            method.Invoke(service, new object[] { args });
+
+            System.Threading.Thread.Sleep(-1);
+
+
+        }
+
+        class CommandFormatException : FormatException
+        {
+            public CommandFormatException(string msg) : base(msg) { }
+        }
+
+        class CommandParser
+        {
+            public Func<string, string[], int> Start { get; set; }
+            public Func<string, int> Stop { get; set; }
+            public Func<string, int> Pause { get; set; }
+            public Func<string, int> Continue { get; set; }
+
+            public CommandParser(System.ServiceProcess.ServiceBase[] services)
+            {
+                Services = services;
+            }
+            System.ServiceProcess.ServiceBase[] Services;
+
+
+            Tuple<string, string[]> ParseArguments(string[] args, bool allowarguments)
+            {
+                if (args.Length == 0)
+                {
+                    if (Services.Length != 1) throw new CommandFormatException("Multiple services are available. Please specify servicename.");
+                    return new Tuple<string, string[]>(Services[0].ServiceName, new string[] { });
+                }
+                else
+                {
+                    if (args.Length > 1 && !allowarguments) throw new CommandFormatException("Command did not expect parameters.");
+                    return new Tuple<string, string[]>(args[0], args.Skip(1).ToArray());
+                }
+            }
+
+            public int Parse(string[] args)
+            {
+
+                var command = args[0];
+
+                var commandarguments = args.Skip(1).ToArray();
+
+                switch (command)
+                {
+                    case "start":
+                        var startargs = ParseArguments(commandarguments, allowarguments: true);
+                        return Start(startargs.Item1, startargs.Item2);
+                    case "stop":
+                        var stopargs = ParseArguments(commandarguments, allowarguments: false);
+                        return Stop(stopargs.Item1);
+                    case "pause":
+                        var pauseargs = ParseArguments(commandarguments, allowarguments: false);
+                        return Pause(pauseargs.Item1);
+                    case "continue":
+                        var continueargs = ParseArguments(commandarguments, allowarguments: false);
+                        return Pause(continueargs.Item1);
+                    default:
+                        throw new CommandFormatException($"Invalid command <{command}>");
+                }
+
+            }
+        }
+
+
+        public static int ExecuteCommand(System.Reflection.Assembly assembly, string[] args, System.ServiceProcess.ServiceBase[] services)
+        {
+            if (args.Length == 0) throw new CommandFormatException("Please specify a command.");
+
+            if (args.Length == 1)
+            {
+                if (args[0] == "list")
+                {
+                    System.Console.WriteLine($"Available services:\n\t{services.Select(x => x.ServiceName).Aggregate((src, next) => $"{src}\n\t{next}")}\n");
+                    return 0;
+                }
+                if (args[0] == "install") return InstallService(assembly);
+                if (args[0] == "uninstall") return UninstallService(assembly);
+                if (args[0] == "startconsole") return InteractiveManager.StartNew(services);
+            }
+
+
+            var parser = new CommandParser(services);
+
+            parser.Start = (servicename, serviceargs) => StartService(servicename, serviceargs);
+            parser.Stop = servicename => StopService(servicename);
+            parser.Pause = servicename => PauseService(servicename);
+            parser.Continue = servicename => ContinueService(servicename);
+
+            return parser.Parse(args);
+        }
+
 
 
         public static int ServiceMain(System.Reflection.Assembly assembly, string[] args, System.ServiceProcess.ServiceBase[] services)
         {
-            if (services.Length == 0)
-            {
-                System.Console.Error.WriteLine("No services registered in assembly");
-                return 0;
-            }
+            if (services.Length == 0) throw new InvalidOperationException("No services in assembly");
 
-            if (args.Length == 1 && args[0] == "install")
+            try
             {
-                return InstallService(assembly);
+                return ExecuteCommand(assembly, args, services);
             }
-            else if (args.Length == 1 && args[0] == "uninstall")
+            catch (CommandFormatException e)
             {
-                return UninstallService(assembly);
-            }
-            else if (args.Length >= 1 && args.Length <= 2 && args[0] == "start")
-            {
-                if (args.Length == 2)
-                {
-                    StartService(args[1]);
-                }
-                else
-                {
-                    if (services.Length == 1) StartService(services.First().ServiceName);
-                    else
-                    {
-                        System.Console.Error.WriteLine(String.Format("Multiple services are available. Please specify which service to start.\nAvailable services:\n{0}",
-                            services.Select(x => x.ServiceName).Aggregate((src, next) => src + "\n" + next)));
-                        return 1;
-                    }
-                }
-            }
-            else if (args.Length >= 1 && args.Length <= 2 && args[0] == "stop")
-            {
-                if (args.Length == 2)
-                {
-                    StopService(args[1]);
-                }
-                else
-                {
-                    if (services.Length == 1)
-                    {
-                        StopService(services.First().ServiceName);
-                    }
-                    else
-                    {
-                        System.Console.Error.WriteLine(String.Format("Multiple services are available. Please specify which service to stop.\nAvailable services:\n{0}",
-                            services.Select(x => x.ServiceName).Aggregate((src, next) => src + "\n" + next)));
-                        return 1;
-                    }
-                }
-            }
-            else if (args.Length == 1 && args[0] == "list")
-            {
-                System.Console.WriteLine($"Available services:\n{services.Select(x => x.ServiceName).Aggregate((src, next) => $"{src}\n{next}")}");              
-            }
-            else if (args.Length > 1 && args[0] == "run")
-            {
-
-                var servicename = args[1];
-                var service = services.FirstOrDefault(x => x.ServiceName == servicename);
-                if (service == null)
-                {
-                    System.Console.WriteLine($"Invalid service name \"{servicename}\".");
-                    return 1;
-                }
-
-                var method = service.GetType().GetMethod("OnStart", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-                method.Invoke(service, new object[] { args.Skip(2).ToArray() });
-
-                System.Threading.Thread.Sleep(-1);
-
-
-                return 0;
-            }
-            else if (args.Length == 0)
-            {
-                System.ServiceProcess.ServiceBase.Run(services);
-            }
-            else
-            {
-                System.Console.WriteLine("Invalid parameter. valid parameters are:\ninstall\nuninstall\nstart [servicename]\nstop [servicename]\nrun [servicename] [parameters...]\nlist");
+                System.Console.WriteLine(
+$@"{e.Message}
+Usage: 
+    install
+    uninstall
+    start [service] [args...]
+    stop [service]
+    pause [service]
+    continue [service]
+    list
+    startconsole");
                 return 1;
             }
-
-
-            return 0;
         }
 
 
         public static int InstallService(System.Reflection.Assembly assembly)
         {
-            try
-            {
-                System.Configuration.Install.AssemblyInstaller Installer = new System.Configuration.Install.AssemblyInstaller(assembly.Location, null);
-                Installer.UseNewContext = true;
-                Installer.Install(null);
-                Installer.Commit(null);
-                return 0;
-            }
-            catch (Exception e)
-            {
-                System.Console.WriteLine("Unable to install service: " + e.Message);
-                return 1;
-            }
+            System.Configuration.Install.AssemblyInstaller Installer = new System.Configuration.Install.AssemblyInstaller(assembly.Location, null);
+            Installer.UseNewContext = true;
+            Installer.Install(null);
+            Installer.Commit(null);
+            return 0;
         }
 
         public static int UninstallService(System.Reflection.Assembly assembly)
         {
-            try
-            {
-                System.Configuration.Install.AssemblyInstaller Installer = new System.Configuration.Install.AssemblyInstaller(assembly.Location, null);
-                Installer.UseNewContext = true;
-                Installer.Uninstall(null);
-                return 0;
-            }
-            catch (Exception e)
-            {
-                System.Console.WriteLine("Unable to uninstall service: " + e.Message);
-                return 1;
-            }
-
+            System.Configuration.Install.AssemblyInstaller Installer = new System.Configuration.Install.AssemblyInstaller(assembly.Location, null);
+            Installer.UseNewContext = true;
+            Installer.Uninstall(null);
+            return 0;
         }
 
-        public static int StartService(string servicename)
+        public static int StartService(string servicename, string[] args)
         {
-            try
-            {
-                System.ServiceProcess.ServiceController sc = new System.ServiceProcess.ServiceController(servicename);
-                sc.Start();
-            }
-            catch (Exception e)
-            {
-                System.Console.WriteLine("Unable to start service: " + e.Message);
-                return 1;
-            }
+            System.ServiceProcess.ServiceController sc = new System.ServiceProcess.ServiceController(servicename);
+            sc.Start();
             return 0;
         }
 
         public static int StopService(string servicename)
         {
-            try
-            {
-                System.ServiceProcess.ServiceController sc = new System.ServiceProcess.ServiceController(servicename);
-                sc.Stop();
-            }
-            catch (Exception e)
-            {
-                System.Console.WriteLine("Unable to stop service: " + e.Message);
-                return 1;
-            }
+            System.ServiceProcess.ServiceController sc = new System.ServiceProcess.ServiceController(servicename);
+            sc.Stop();
             return 0;
         }
+
+        public static int PauseService(string servicename)
+        {
+            System.ServiceProcess.ServiceController sc = new System.ServiceProcess.ServiceController(servicename);
+            sc.Pause();
+            return 0;
+        }
+
+        public static int ContinueService(string servicename)
+        {
+            System.ServiceProcess.ServiceController sc = new System.ServiceProcess.ServiceController(servicename);
+            sc.Continue();
+            return 0;
+        }
+
+
+
+        class InteractiveManager
+        {
+
+            public InteractiveManager(System.ServiceProcess.ServiceBase[] services)
+            {
+                Services = services;
+                Parser = new CommandParser(services);
+
+                Parser.Start = (name, args) =>
+                {
+                    StartAsync(name, args).ContinueWith(task => ShowException(task.Exception));
+                    return 0;
+                };
+                Parser.Stop = name =>
+                {
+                    StopAsync(name).ContinueWith(task => ShowException(task.Exception));
+                    return 0;
+                };
+                Parser.Pause = name =>
+                {
+                    PauseAsync(name).ContinueWith(task => ShowException(task.Exception));
+                    return 0;
+                };
+                Parser.Continue = name =>
+                {
+                    ContinueAsync(name).ContinueWith(task => ShowException(task.Exception));
+                    return 0;
+                };
+            }
+
+            System.ServiceProcess.ServiceBase[] Services;
+
+            System.ServiceProcess.ServiceBase GetService(string name)
+            {
+                var ret = Services.FirstOrDefault(x => x.ServiceName == name);
+                if (ret == null) throw new CommandFormatException($"Service \"{name}\" is not available.");
+                return ret;
+            }
+
+            CommandParser Parser;
+            Dictionary<string, Task> runningservices = new Dictionary<string, Task>();
+
+            object reportlocker = new object();
+            void SendReport(string msg)
+            {
+                lock (reportlocker)
+                {
+                    System.Console.WriteLine(msg);
+                }
+            }
+            void ShowException(Exception e)
+            {
+                if (e == null) return;
+                if (e is AggregateException)
+                {
+                    foreach (var sube in ((AggregateException)e).InnerExceptions)
+                    {
+                        ShowException(sube);
+                    }
+                }
+                else
+                    SendReport($"{e.GetType().Name}: {e.Message}");
+            }
+
+
+            async Task StartAsync(string servicename, string[] args)
+            {
+                Task servicetask;
+                lock (runningservices)
+                {
+                    if (runningservices.ContainsKey(servicename)) throw new InvalidOperationException($"Service \"{servicename}\" is already started.");
+                    var service = GetService(servicename);
+                    var method = service.GetType().GetMethod("OnStart", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                    SendReport($"Service {servicename} is starting.");
+                    servicetask = Task.Run(() => method.Invoke(service, new object[] { args }));
+                    runningservices[servicename] = servicetask;
+                }
+                await servicetask;
+                lock (runningservices)
+                {
+                    runningservices.Remove(servicename);
+                }
+                SendReport($"Service {servicename} stopped.");
+            }
+
+            async Task StopAsync(string servicename)
+            {
+                var service = GetService(servicename);
+                Task servicetask;
+                lock (runningservices)
+                {
+                    if (!runningservices.ContainsKey(servicename)) throw new KeyNotFoundException($"Service {servicename} is not running.");
+                    var method = service.GetType().GetMethod("OnStop", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                    servicetask = Task.Run(() => method.Invoke(service, new object[] { }));
+                }
+                await servicetask;
+                SendReport($"Stop signal sent to {servicename}.");
+            }
+
+            async Task PauseAsync(string servicename)
+            {
+                var service = GetService(servicename);
+                Task servicetask;
+                lock (runningservices)
+                {
+                    if (!runningservices.ContainsKey(servicename)) throw new KeyNotFoundException($"Service {servicename} is not running.");
+                    var method = service.GetType().GetMethod("OnPause", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                    servicetask = Task.Run(() => method.Invoke(service, new object[] { }));
+                }
+                await servicetask;
+                SendReport($"Pause signal sent to {servicename}.");
+            }
+
+            async Task ContinueAsync(string servicename)
+            {
+                var service = GetService(servicename);
+                Task servicetask;
+                lock (runningservices)
+                {
+                    if (!runningservices.ContainsKey(servicename)) throw new KeyNotFoundException($"Service {servicename} is not running.");
+                    var method = service.GetType().GetMethod("OnContinue", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                    servicetask = Task.Run(() => method.Invoke(service, new object[] { }));
+                }
+                await servicetask;
+                SendReport($"Continue signal sent to {servicename}.");
+            }
+
+
+
+
+            public int Run()
+            {
+                while (true)
+                {
+                    var args = System.Console.ReadLine().Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                    if (args.Length == 1)
+                    {
+                        if (args[0] == "quit") break;
+                    }
+                    try
+                    {
+                        Parser.Parse(args);
+                    }
+                    catch (Exception e)
+                    {
+                        ShowException(e);
+                    }
+                }
+                return 0;
+            }
+
+            public static int StartNew(System.ServiceProcess.ServiceBase[] services) => new InteractiveManager(services).Run();
+
+        }
+
 
 
     }
@@ -235,3 +408,4 @@ namespace Xintric.MUtil
 
 
 }
+
